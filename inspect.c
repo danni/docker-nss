616b960e47874afcea3f3ebd2e83d0ead1cc5927
Arjan van der Velde <vandervelde.ag@gmail.com>
2015-07-14 11:53:05 -0400

    a bit of a hack to get user outside of the docker group to use this.

---
+++ inspect.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+	if (argc > 1) {
+		const char *myargs[] = {"/usr/bin/docker", "inspect", "--format={{.NetworkSettings.IPAddress}}", argv[1] };
+        	return execlp(myargs[0], myargs[0], myargs[1], myargs[2], myargs[3], NULL);
+	}
+	return 0;
+}
+
